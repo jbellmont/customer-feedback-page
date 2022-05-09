1b37604d3fc1a91de481a338de898ea4646e5850
Jack Bellmont <bellmont@gmail.com>
2022-05-09 17:43:01 +0100

    Extend eslint with React specific configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,26 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'google', 'prettier'],
+  overrides: [
+    {
+      extends: [
+        'plugin:jest-dom/recommended',
+        'plugin:jsx-a11y/recommended',
+        'plugin:testing-library/react',
+      ],
     },
-    "extends": [
-        "plugin:react/recommended",
-        "google"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-}
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {'require-jsdoc': 0, 'react/react-in-jsx-scope': 0},
+};
