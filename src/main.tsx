fc411e48a97e32ae15b34acaa73c1887634eb353
Jack Bellmont <bellmont@gmail.com>
2022-05-10 15:11:33 +0100

    Add custom font

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// Roboto font for Material-UI.
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import App from './features/app/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <App>
+      <h1>Hello, world!</h1>
+    </App>
   </React.StrictMode>
 );
