92be0d3406449aea03ff342290a04e3bcc02432c
Jack Bellmont <bellmont@gmail.com>
2022-05-12 08:03:28 +0100

    Implement sort by latest review logic

--- src/features/customer-feedback/components/ReviewsList.tsx
+++ src/features/customer-feedback/components/ReviewsList.tsx
@@ -5,7 +5,33 @@ interface ReviewsListProps {
 }
 
 const ReviewsList = ({reviews}: ReviewsListProps) => {
-  return <div>Reviews List component</div>;
+  const sortReviewsByLatestDate = (reviews: Review[]): Review[] => {
+    return [...reviews].sort((a, b) => {
+      return b.date.getTime() - a.date.getTime();
+    });
+  };
+
+  const transformReviews = (reviews: Review[]): Review[] => {
+    // Add more array transformation functions to the 'pipeline', e.g. filter.
+    return sortReviewsByLatestDate(reviews);
+  };
+
+  const reviewsToDisplay: Review[] = transformReviews(reviews);
+
+  // TODO(jackbellmont): Handle no reviews (e.g. 'No reviews submitted yet').
+  // TODO(jackbellmont): Handle JB avatar.
+  // TODO(jackbellmont): Format date nicely, e.g. Tues 15th March.
+  return (
+    <ul>
+      {reviewsToDisplay.map((review: Review) => {
+        return (
+          <li key={review.id}>
+            {review.name} - {review.date.toLocaleDateString()}
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default ReviewsList;
