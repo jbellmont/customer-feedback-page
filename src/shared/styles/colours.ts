4dbd3d534504b3a111e0b4f449d1ee331d015d3e
Jack Bellmont <bellmont@gmail.com>
2022-05-13 18:32:59 +0100

    Style Success Message

--- src/shared/styles/colours.ts
+++ src/shared/styles/colours.ts
@@ -3,5 +3,7 @@ export const ratingStarYellow = '#faaf00';
 export const googleDarkGrey = '#c4c4c4';
 export const googleLightGrey = '#e2e2e2';
 
+export const googleGreen = '#388e3c';
+
 // Included to pass a11y colour-constrast issues.
 export const avatarDarkGrey = '#595959';
